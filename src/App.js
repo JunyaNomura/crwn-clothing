b7814afa8f475b6539aa7ced1d46f8b328a98356
junya.nomura <junya.nomura@administratornoMacBook-Air.local>
2021-08-10 04:27:32 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import HomePage from './homepage.component'
+
+const App = () => {
+  return (
+    <>
+    <HomePage />
+    </>
+  );
+}
+
+export default App;
